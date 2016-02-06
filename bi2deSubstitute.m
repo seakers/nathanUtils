ffbbb0d75ef8bab3a009197ecd0be06aff7d4d3c
Nathan Knerr <importN@hotmail.com>
2016-02-06 17:42:20 -0500

    Made bi2deSubstitute actually implement the left-msb standard it supposedly implements.

--- bi2deSubstitute.m
+++ bi2deSubstitute.m
@@ -1,7 +1,7 @@
 %%turns binary row vectors into decimal numbers.
 %%little endian (left-msb)
 function [nums] = bi2deSubstitute(binaries)
-binaryKeys=repmat(2.^(0:fliplr(size(binaries,2)-1)),size(binaries,1),1);
+binaryKeys=repmat(2.^fliplr(0:(size(binaries,2)-1)),size(binaries,1),1);
 nums=sum(binaryKeys.*binaries,2);
 end
 
